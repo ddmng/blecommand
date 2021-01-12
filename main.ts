ce3ae7d765b2f475896f627d67ee730c60a714fd
ddmng <daniele.diminica@gmail.com>
2021-01-12 17:54:18 +0100

    uart response

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Dollar), function () {
     command = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Dollar))
-    serial.writeLine("received command:" + command.substr(0, 1))
-    if (command.substr(0, 1) == "9") {
+    command_code = command.split(":")
+    serial.writeLine("command code:" + command_code)
+    if (command_code[0] == "P") {
         serial.writeLine("requested password")
-        cmdPassword(command.substr(1, 1))
-    } else if (command.substr(0, 1) == "1") {
+        cmdPassword(command_code[1])
+    } else if (command_code[0] == "1") {
         serial.writeLine("requested pin activation")
         activatePin()
     } else {
@@ -17,9 +18,9 @@ bluetooth.onBluetoothConnected(function () {
     serial.writeLine("waiting for password")
     basic.showLeds(`
         . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
+        . . # . .
+        # . # . #
+        . # # # .
         . . # . .
         `)
 })
@@ -36,6 +37,9 @@ bluetooth.onBluetoothDisconnected(function () {
     bluetooth.stopAdvertising()
     basicBLEAdvertise()
 })
+input.onButtonPressed(Button.A, function () {
+    bluetooth.uartWriteNumber(9)
+})
 function publishBLEServices () {
     bluetooth.startButtonService()
 }
@@ -46,6 +50,7 @@ function basicBLEAdvertise () {
     7,
     true
     )
+    bluetooth.startUartService()
     basic.showLeds(`
         # . # # .
         . # # . #
@@ -53,11 +58,13 @@ function basicBLEAdvertise () {
         . # # . #
         # . # # .
         `)
-    bluetooth.startUartService()
+}
+function setPin () {
+	
 }
 function cmdPassword (pwd: string) {
     serial.writeLine("received password:" + pwd)
-    if (pwd == "9") {
+    if (pwd == SYS_PASSWORD) {
         serial.writeLine("password ok")
         ble_connected = 2
         publishBLEServices()
@@ -68,10 +75,10 @@ function cmdPassword (pwd: string) {
             # . # . .
             . # . . .
             `)
+        bluetooth.uartWriteNumber(0)
     } else {
-        let password = 0
-        serial.writeLine("password ko")
-        serial.writeLine("" + (password))
+        serial.writeLine("wrong password:" + pwd)
+        bluetooth.uartWriteNumber(1)
         bluetooth.stopAdvertising()
         basicBLEAdvertise()
     }
@@ -80,14 +87,20 @@ function activatePin () {
     serial.writeLine("activatePin")
     if (ble_connected == 2) {
         serial.writeLine("activating...")
+        bluetooth.uartWriteNumber(0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
     } else {
-        serial.writeLine("denied")
+        serial.writeLine("access denied")
+        bluetooth.uartWriteNumber(1)
     }
 }
 let ble_connected = 0
+let command_code: string[] = []
 let command = ""
+let SYS_PASSWORD = ""
 serial.writeLine("started")
 basicBLEAdvertise()
+SYS_PASSWORD = "AAAA"
 basic.forever(function () {
 	
 })
